1d43a66cda55041049759169d03ff5d7c0d497bf
AshishSinghDeveloper <ashishsng21@hotmail.com>
2020-10-07 19:20:43 -0500

    Adds logic to implement Add and delete items

--- spa-ang/src/app/app.component.ts
+++ spa-ang/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { toUnicode } from 'punycode';
+import { Todo} from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Single Page';
-  name = "Ashish Singh"
+  name = "Ashish Singh";
+
+  todoValue: string;
+  list: Todo[];
+
+  ngOnInit(){
+    this.list = [];
+    this.todoValue = "";
+  }
+
+  AddItem(){
+    if(this.todoValue !== null){
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue="";    
+  }
+
+  DeleteItem(id: number){
+    this.list = this.list.filter(item => item.id !== id);
+  }
+
 }
+
